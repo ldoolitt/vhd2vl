88d6a4044e4125ea0a0c011e0a1223e365f9a868
Larry Doolittle <ldoolitt@recycle.lbl.gov>
2017-11-20 15:12:24 -0800

    Adding support for while loop
    Supplied by jeinstei
    Labelling of the loop is still unsupported.

---
+++ translated_examples/while.v
@@ -0,0 +1,26 @@
+// no timescale needed
+
+module whilep(
+input wire [31:0] A,
+output reg [3:0] Z
+);
+
+
+
+
+
+  always @(A) begin : P1
+    reg [31:0] I;
+
+    Z <= 4'b0000;
+    I = 0;
+    while ((I <= 3)) begin
+      if((A == I)) begin
+        Z[I] <= 1'b1;
+      end
+      I = I + 1;
+    end
+  end
+
+
+endmodule
